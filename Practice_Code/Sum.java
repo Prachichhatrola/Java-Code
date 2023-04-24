446b052d7dd44f4e2bc852c51c2099bd101f55c6
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-04-24 19:39:04 +0530

    Add files via upload

---
+++ Practice_Code/Sum.java
@@ -0,0 +1,9 @@
+public class Sum {
+    public static void main(String[] args){
+        int num1 = 6;
+        int num2 = 5;
+        int num3 = 7;
+        int sum = num1 + num2+ num3;
+        System.out.println(sum);
+    }
+}
