6f3e669b3266c6eb776e1c7cad60e49a847a8f15
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-04-25 07:46:40 +0530

    Add files via upload

---
+++ Practice_Code/literals.java
@@ -0,0 +1,15 @@
+public class literals {
+    public static void main(String[] args){
+        byte age = 34;
+        int age2 = 56;
+        short age3 = 87;
+        long ageDino = 566666666666L;
+        char ch = 'A';
+        float f1 = 5.6f;
+        double d1 = 4.66;
+        boolean a = true;
+        System.out.println(age);
+        String str = "Prachi";
+        System.out.println(str);
+    }
+}
