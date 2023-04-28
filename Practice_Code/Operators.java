fa492b2e3c9678233019eb8f53bcbcfa2cdb778d
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-04-28 08:36:41 +0530

    Add files via upload

---
+++ Practice_Code/Operators.java
@@ -0,0 +1,23 @@
+public class Operators {
+    public static void main(String[] args) {
+        //1. Arithmetic operators 
+        int a = 4;
+        int b = 6 + a;
+        int b = 6 % a;
+
+        //2. Assignment operators
+        int b = 5;
+        b *= 3;
+        System.out.println(b);
+
+        //3. Comparison operators
+        System.out.println(64<6);
+
+        //4. Logical Operators
+        System.out.println(64>4 && 64>7);
+        System.out.println(64>4 || 64>7);
+
+        //5. Bitwise operators
+        System.out.println(2&3);
+    }
+}
