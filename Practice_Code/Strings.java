d28fa805b40f49aa0cafa08a21a35cfcf6e52aa8
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-05-07 16:49:58 +0530

    Add files via upload

---
+++ Practice_Code/Strings.java
@@ -0,0 +1,7 @@
+public class Strings {
+    public static void main(String[] args) {
+        // String name = new String(original:"Prachi");
+        String name = "Prachi";
+        System.out.println(name);
+    }
+}
