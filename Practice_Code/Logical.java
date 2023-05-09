0bf2fb55a9789ab031df2b7d1fba4f42a38c4021
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-05-09 10:17:04 +0530

    Add files via upload

---
+++ Practice_Code/Logical.java
@@ -0,0 +1,30 @@
+public class Logical {
+    public static void main(String[] args) {
+        System.out.println("For Logical AND...");
+        boolean a = true;
+        boolean b = false;
+        // if (a && b){
+        //     System.out.println("Y");
+        // }
+        // else{
+        //     System.out.println("N");
+        // }
+
+        System.out.println("For Logical OR...");
+        // boolean a = true;
+        // boolean b = false;
+        // if (a || b){
+        //     System.out.println("Y");
+        // }
+        // else{
+        //     System.out.println("N");
+        // }
+
+       System.out.println("For Logical NOT...");
+       System.out.print("Not(a) is ");
+       System.out.println(!a);
+       System.out.print("Not(b) is ");
+       System.out.println(!b);
+        }
+    }
+
