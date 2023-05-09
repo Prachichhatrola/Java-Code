e4ef719bb45e82ad9520bc946db5387c390c9d13
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-05-09 09:53:55 +0530

    Add files via upload

---
+++ Practice_Code/Conditionals.java
@@ -0,0 +1,11 @@
+public class Conditionals {
+    public static void main(String[] args) {
+        int age = 18;
+        if (age>18){
+            System.out.println("Yes you can drive!");
+        }
+        else{
+            System.out.println("No boy you can not drive yet!");
+        }
+    }
+}
