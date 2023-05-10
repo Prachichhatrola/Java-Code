f3c9b722891d4644446a4c77e202dd1763c04514
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-05-10 10:42:06 +0530

    Add files via upload

---
+++ Practice_Code/do_while.java
@@ -0,0 +1,18 @@
+public class do_while {
+    public static void main(String[] args) {
+
+        //while loop
+        int a = 0;
+        while(a<5){
+            System.out.println(a);
+            a++;
+        }
+        
+        //do while loop
+        int b = 0;
+        do{
+            System.out.println(b);
+            b++;
+        }while(b<5);
+    }
+}
