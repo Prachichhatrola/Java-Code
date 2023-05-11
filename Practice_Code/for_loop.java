aa402ba12b26976b1ab28c2c3275885714896ae4
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-05-11 18:28:30 +0530

    Add files via upload

---
+++ Practice_Code/for_loop.java
@@ -0,0 +1,19 @@
+public class for_loop {
+    public static void main(String[] args) {
+        // for(int i=0; i<=10; i++){
+        //     System.out.println(i);
+        // }
+
+          // 2n = Even Numbers = 0, 2, 4, 6, 8...
+          // 2n+1 = Odd Numbers = 1, 3, 5, 7, 9...
+
+        //   int n = 5;
+        //   for(int i=0; i<n; i++){
+        //     System.out.println(2*i+1);
+        //   }
+
+        for(int i=5; i>=0; i--){
+            System.out.println(i);
+        }
+    }
+}
