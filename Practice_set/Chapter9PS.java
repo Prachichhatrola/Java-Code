dce6f1b33c64ccf3392dc9e45d5355f71ae56ab2
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-06-08 11:16:50 +0530

    Add files via upload

---
+++ Practice_set/Chapter9PS.java
@@ -0,0 +1,81 @@
+package com.company;
+
+class Cylinder {
+    private int radius;
+    private int height;
+
+    public Cylinder(int radius, int height) {
+        this.radius = radius;
+        this.height = height;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public double surfaceArea() {
+        return 2 * Math.PI * radius * radius + 2 * Math.PI * radius * height;
+    }
+
+    public double volume() {
+        return Math.PI * radius * radius * height;
+    }
+
+}
+
+class Rectangle {
+    private int length;
+    private int breadth;
+
+    public Rectangle() {
+        this.length = 4;
+        this.breadth = 5;
+    }
+
+    public Rectangle(int length, int breadth) {
+        this.length = length;
+        this.breadth = breadth;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public int getBreadth() {
+        return breadth;
+    }
+}
+
+public class Chapter9PS {
+
+    public static void main(String[] args) {
+        /*
+         * // Problem 1
+         * Cylinder myCylinder = new Cylinder(9, 12);
+         * //myCylinder.setHeight(12);
+         * System.out.println(myCylinder.getHeight());
+         * //myCylinder.setRadius(9);
+         * System.out.println(myCylinder.getRadius());
+         * // Problem 2
+         * System.out.println(myCylinder.surfaceArea());
+         * System.out.println(myCylinder.volume());
+         */
+
+        // Problem 3
+        Rectangle r = new Rectangle(12, 56);
+        System.out.println(r.getLength());
+        System.out.println(r.getBreadth());
+    }
+}
