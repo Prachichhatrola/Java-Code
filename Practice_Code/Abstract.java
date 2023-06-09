1c1477736ced27320310cfe4f2edd2e8f682ac8c
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-06-09 19:54:41 +0530

    Add files via upload

---
+++ Practice_Code/Abstract.java
@@ -0,0 +1,41 @@
+package com;
+
+abstract class Parent2 {
+    public Parent2() {
+        System.out.println("Mai base2 ka constructor hoon");
+    }
+
+    public void sayHello() {
+        System.out.println("Hello");
+    }
+
+    abstract public void greet();
+
+    abstract public void greet2();
+}
+
+class Child2 extends Parent2 {
+    @Override
+    public void greet() {
+        System.out.println("Good Morning");
+    }
+
+    @Override
+    public void greet2() {
+        System.out.println("Good Afternoon");
+    }
+}
+
+abstract class Child3 extends Parent2 {
+    public void th() {
+        System.out.println("I am good");
+    }
+}
+
+public class Abstract {
+    public static void main(String[] args) {
+        // Parent2 p = new Parent2(); --> error
+        Child2 c = new Child2();
+        // Child3 c3 = new Child3(); --> error
+    }
+}
