b26515e40c5c071c6a03939712b93be5f59190d0
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-06-10 14:19:50 +0530

    Add files via upload

---
+++ Practice_Code/inheritance_interface.java
@@ -0,0 +1,40 @@
+package com.company;
+
+interface sampleInterface {
+    void meth1();
+
+    void meth2();
+}
+
+interface childSampleInterface extends sampleInterface {
+    void meth3();
+
+    void meth4();
+}
+
+class MySampleClass implements childSampleInterface {
+    public void meth1() {
+        System.out.println("meth1");
+    }
+
+    public void meth2() {
+        System.out.println("meth2");
+    }
+
+    public void meth3() {
+        System.out.println("meth3");
+    }
+
+    public void meth4() {
+        System.out.println("meth4");
+    }
+}
+
+public class inheritance_interface {
+    public static void main(String[] args) {
+        MySampleClass obj = new MySampleClass();
+        obj.meth1();
+        obj.meth2();
+        obj.meth3();
+    }
+}
