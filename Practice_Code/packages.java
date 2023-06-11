58e187f0136a0db5342121033c66232450864c88
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-06-11 11:00:18 +0530

    Add files via upload

---
+++ Practice_Code/packages.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.Scanner;
+//import java.util.*;
+public class packages {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // java.util.Scanner sc = new java.util.Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println("This is my scanner taking input as " + a);
+    }
+}
