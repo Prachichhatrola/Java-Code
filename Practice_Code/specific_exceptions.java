962a8cdc53f1667ef72e6d4dfb2fb096f0af83fe
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-06-12 09:31:19 +0530

    Add files via upload

---
+++ Practice_Code/specific_exceptions.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class specific_exceptions {
+    public static void main(String[] args) {
+        int [] marks = new int[3];
+        marks[0] = 7;
+        marks[1] = 56;
+        marks[2] = 6;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the array index");
+        int ind = sc.nextInt();
+
+        System.out.println("Enter the number you want to divide the value with");
+        int number = sc.nextInt();
+        try{
+            System.out.println("The value at array index entered is: " + marks[ind]);
+            System.out.println("The value of array-value/number is: " + marks[ind]/number);
+        }
+        catch (ArithmeticException e){
+            System.out.println("ArithmeticException occured!");
+            System.out.println(e);
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("ArrayIndexOutOfBoundsException occured!");
+            System.out.println(e);
+        }
+        catch (Exception e){
+            System.out.println("Some other exception occured!");
+            System.out.println(e);
+        }
+    }
+}
