1c93dc03c1f293b324d64eb7ed39423621227228
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-06-13 12:05:15 +0530

    Add files via upload

---
+++ Practice_Code/date_class.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Date;
+
+public class date_class {
+    public static void main(String[] args) {
+//        System.out.println(Long.MAX_VALUE);
+//        System.out.println(System.currentTimeMillis());
+        Date d = new Date();
+        System.out.println(d);
+        System.out.println(d.getTime());
+        System.out.println(d.getDate());
+        System.out.println(d.getSeconds());
+        System.out.println(d.getYear());
+    }
+}
