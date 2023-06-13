d2b7ba289155cea41b94cb3a645d1a8efdbb069f
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-06-13 11:37:28 +0530

    Add files via upload

---
+++ Practice_Code/arraydeque.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.ArrayDeque;
+
+public class arraydeque {
+    public static void main(String[] args) {
+        ArrayDeque<Integer> ad1 = new ArrayDeque<>();
+        ad1.add(6);
+        ad1.add(56);
+        ad1.add(9);
+        ad1.addFirst(5);
+        System.out.println(ad1.getFirst());
+        System.out.println(ad1.getLast());
+    }
+}
