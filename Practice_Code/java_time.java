5f0bcb4a0a930328c16e3b45b245d31787c1abcd
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-06-13 12:30:27 +0530

    Add files via upload

---
+++ Practice_Code/java_time.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class java_time {
+    public static void main(String[] args) {
+        LocalDate d = LocalDate.now();
+        System.out.println(d);
+
+        LocalTime t = LocalTime.now();
+        System.out.println(t);
+
+        LocalDateTime dt = LocalDateTime.now();
+        System.out.println(dt);
+    }
+}
