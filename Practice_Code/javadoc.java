d7fb28800c35c943935a24b3c4cf6f7355fe7919
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-06-13 15:38:19 +0530

    Add files via upload

---
+++ Practice_Code/javadoc.java
@@ -0,0 +1,21 @@
+package com.company;
+/**
+ * This class is to demonstrate what javadoc is and how it is used in the java industry
+ * This is <i>italic</i> word<p>this is a new paragraph</p>
+ * @author Harry (CodeWithHarry)
+ * @version 0.1
+ * @since 2002
+ * @see <a href="https://docs.oracle.com/en/java/javase/14/docs/api/index.html" target="_blank">Java Docs</a>
+ */
+
+public class javadoc {
+
+    public void add(int a, int b){
+        System.out.println("The sum is: " + a+b);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("This is my main method");
+    }
+
+}
