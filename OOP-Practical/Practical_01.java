3b9e5c99cbb06766c8a33c0d0692ec8fb2b67fd6
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-06-14 10:49:14 +0530

    Update Practical_01.java

--- OOP-Practical/Practical_01.java
+++ OOP-Practical/Practical_01.java
@@ -1,1 +1,11 @@
+public class Practical_01 {
+    public static void main(String[] args) {
+        String firstName = "Prachi";
+        String secondName = "Dineshbhai";
+        String lastName = "Chhatrola";
 
+        System.out.println("First Name: " + firstName);
+        System.out.println("Second Name: " + secondName);
+        System.out.println("Last Name: " + lastName);
+    }
+}
