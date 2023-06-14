49fb897ae5a510044ef3391d8bc857d102a3a942
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-06-14 17:10:12 +0530

    Add files via upload

---
+++ OOP-Practical/Practical_17.java
@@ -0,0 +1,18 @@
+public class Practical_17 {
+    public static void main(String[] args) {
+        String inputString = "Hello, World";
+
+        // Get the length of the input string
+        int length = inputString.length();
+
+        // Check if the length is less than or equal to 10
+        if (length <= 10) {
+            System.out.println("Substring: " + inputString);
+        } else {
+            // Get the substring of the last ten characters
+            String substring = inputString.substring(length - 10);
+
+            System.out.println("Substring: " + substring);
+        }
+    }
+}
