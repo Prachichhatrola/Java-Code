7b676c9017ccb8be2bbf46848b17a816464e221a
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-06-14 11:38:14 +0530

    Add files via upload

---
+++ OOP-Practical/Practical_05.java
@@ -0,0 +1,11 @@
+public class Practical_05 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 4; j > i; j--) {
+                System.out.print("A ");
+            }
+            System.out.println("");
+        }
+    }
+}
