fa0275313ffd07901e4c40f009f87ee33c5b4b54
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-06-14 15:16:14 +0530

    Add files via upload

---
+++ OOP-Practical/Practical_11.java
@@ -0,0 +1,15 @@
+public class Practical_11 {
+    public static void main(String[] args) {
+        // Create an array with five Float values
+        Float[] floatArray = { 1.2f, 3.4f, 5.6f, 7.8f, 9.0f };
+
+        // Display the length of the array
+        System.out.println("Length of the array: " + floatArray.length);
+
+        // Display the elements of the array
+        System.out.println("Elements of the array:");
+        for (Float value : floatArray) {
+            System.out.println(value);
+        }
+    }
+}
