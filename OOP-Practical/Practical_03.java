c9b4df5999cca4dbe3882350de97617e27ab5ac9
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-06-14 11:12:31 +0530

    Add files via upload

---
+++ OOP-Practical/Practical_03.java
@@ -0,0 +1,14 @@
+public class Practical_03 {
+    public static void main(String[] args) {
+        int base = 2;
+        int exponent = 12;
+
+        int result = 1;
+        for (int i = 1; i <= exponent; i++) {
+            result *= base;
+        }
+
+        System.out.println("2 raised to the power of 12 is: " + result);
+    }
+}
+
