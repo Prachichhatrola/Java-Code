fdc9608bea74f6fd15dd5f9f3f908be44918ae78
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-06-14 12:01:27 +0530

    Add files via upload

---
+++ OOP-Practical/Practical_06.java
@@ -0,0 +1,10 @@
+public class Practical_06 {
+    public static void main(String[] args) {
+        double num1 = 3.5;
+        double num2 = 2.0;
+
+        double product = num1 * num2;
+
+        System.out.println("Product: " + product);
+    }
+}
