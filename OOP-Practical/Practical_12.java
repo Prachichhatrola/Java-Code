99c5c05082e4fc570d361a18197750cbe5e09d78
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-06-14 16:22:42 +0530

    Add files via upload

---
+++ OOP-Practical/Practical_12.java
@@ -0,0 +1,18 @@
+public class Practical_12 {
+    public static void main(String[] args) {
+        // Create and initialize a byte array
+        byte[] byteArray = { 10, 20, 30, 40 };
+
+        // Calculate the sum of array elements
+        int sum = 0;
+        for (byte num : byteArray) {
+            sum += num;
+        }
+
+        // Calculate the average
+        double average = (double) sum / byteArray.length;
+
+        // Display the average
+        System.out.println("Average of the byte array: " + average);
+    }
+}
