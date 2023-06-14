98a015f22310ff2d61898751d8c2f6e2dc791613
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-06-14 16:26:28 +0530

    Add files via upload

---
+++ OOP-Practical/Practical_13.java
@@ -0,0 +1,24 @@
+public class Practical_13 {
+    public static void main(String[] args) {
+        int[] numbers = { 10, 20, 30, 40, 50 };
+        int searchElement = 30;
+
+        boolean found = false;
+
+        // Search for the element using for-each style loop
+        for (int number : numbers) {
+            if (number == searchElement) {
+                found = true;
+                break;
+            }
+        }
+
+        // Display the result
+        if (found) {
+            System.out.println("Element " + searchElement + " found in the array.");
+        } 
+        else {
+            System.out.println("Element " + searchElement + " not found in the array.");
+        }
+    }
+}
