0025ac964a06a96be5c7d490e6d332c1951a95b0
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-06-14 14:34:14 +0530

    Add files via upload

---
+++ OOP-Practical/Practical_08.java
@@ -0,0 +1,12 @@
+public class Practical_08 {
+    public static void main(String[] args) {
+        System.out.println("Number\tSquare\tCube");
+
+        for (int number = 1; number <= 10; number++) {
+            int square = number * number;
+            int cube = number * number * number;
+
+            System.out.println(number + "\t" + square + "\t" + cube);
+        }
+    }
+}
