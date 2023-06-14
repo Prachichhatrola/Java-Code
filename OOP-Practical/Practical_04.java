0f90e6ee102f57e3f47a73ce8fbb4fa510c5157e
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-06-14 11:19:56 +0530

    Add files via upload

---
+++ OOP-Practical/Practical_04.java
@@ -0,0 +1,24 @@
+public class Practical_04 {
+    public static void main(String[] args) {
+        int number = 29;  // Number to check for primality
+
+        boolean isPrime = true;
+
+        if (number <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i <= Math.sqrt(number); i++) {
+                if (number % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+    }
+}
