b0620ef065c06b64db304164c1ef9322b40fb0c2
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-06-14 11:04:55 +0530

    Add files via upload

---
+++ OOP-Practical/Practical_02.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Practical_02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is even.");
+        } else {
+            System.out.println(number + " is odd.");
+        }
+
+        scanner.close();
+    }
+}
+
