ae4a352ec9ba4872681af3cdcb8a8485a620486e
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-06-14 14:51:44 +0530

    Add files via upload

---
+++ OOP-Practical/Practical_10.java
@@ -0,0 +1,20 @@
+public class Practical_10 {
+    public static int fibonacci(int n) {
+        if (n == 0 || n == 1)
+            return n;
+        else {
+            int x = fibonacci(n - 1) + fibonacci(n - 2);
+            return x;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int fibonacci_len = 15;
+        System.out.print("Fibonacci Series of " + fibonacci_len + " numbers is: \n");
+
+        for (int i = 0; i <= fibonacci_len; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+    }
+}
