5f69c7d2976fef4809b149e73afc8f1cdc2c93c8
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-06-15 09:51:13 +0530

    Add files via upload

---
+++ OOP-Practical/Practical_24.java
@@ -0,0 +1,15 @@
+class circle{
+    float redius;
+    float pie = 3.14f;
+
+    public float area(){
+        return (pie * redius *redius);
+    }
+}
+public class Practical_24 {
+    public static void main(String[] args) {
+        circle crl = new circle();
+        crl.redius = 5;
+        System.out.println(crl.area());
+    }
+}
