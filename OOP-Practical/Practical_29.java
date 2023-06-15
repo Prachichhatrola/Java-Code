d34150b19ea184f245b7f6e4910cae79b543aa38
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-06-15 10:32:29 +0530

    Add files via upload

---
+++ OOP-Practical/Practical_29.java
@@ -0,0 +1,51 @@
+class Circle2 {
+    protected double radius;
+
+    public Circle2(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+}
+
+class Cylinder extends Circle2 {
+    private double height;
+
+    public Cylinder(double radius, double height) {
+        super(radius);
+        this.height = height;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    @Override
+    public double calculateArea() {
+        // Calculate the lateral surface area of the cylinder
+        double baseArea = super.calculateArea();
+        double lateralArea = 2 * Math.PI * radius * height;
+        return 2 * baseArea + lateralArea;
+    }
+
+    public double calculateVolume() {
+        return super.calculateArea() * height;
+    }
+}
+
+public class Practical_29 {
+    public static void main(String[] args) {
+        Circle2 circle = new Circle2(5.0);
+        System.out.println("Circle Area: " + circle.calculateArea());
+
+        Cylinder cylinder = new Cylinder(3.0, 8.0);
+        System.out.println("Cylinder Area: " + cylinder.calculateArea());
+        System.out.println("Cylinder Volume: " + cylinder.calculateVolume());
+    }
+}
