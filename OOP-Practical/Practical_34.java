bc43fa460a3cb9a5e9227a7841bdf8e8bca2ddc7
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-06-15 10:58:37 +0530

    Add files via upload

---
+++ OOP-Practical/Practical_34.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Practical_34 {
+    public static void main(String[] args) {
+        try {
+            Scanner scanner = new Scanner(System.in);
+            
+            System.out.print("Enter a number: ");
+            String input = scanner.nextLine();
+            
+            // Parsing the number, which can throw NumberFormatException
+            int number = Integer.parseInt(input);
+            
+            System.out.println("Entered number: " + number);
+            
+            scanner.close();
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input format! Please enter a valid integer.");
+        }
+    }
+}
