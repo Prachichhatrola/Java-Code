45e9c68f384a66cf769dcc25cdc5532baac2aa75
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-06-15 10:45:50 +0530

    Add files via upload

---
+++ OOP-Practical/Practical_32.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Practical_32 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first integer: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter the second integer: ");
+        int num2 = scanner.nextInt();
+
+        try {
+            int result = divideNumbers(num1, num2);
+            System.out.println("Result of division: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("Error: Division by zero is not allowed.");
+        }
+        scanner.close();
+    }
+
+    public static int divideNumbers(int num1, int num2) {
+        return num1 / num2;
+    }
+}
