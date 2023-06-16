ffa06d578d62c4ce1686604ee7ef746ca1819089
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-06-16 10:06:32 +0530

    Update Practical_06.java

--- OOP-Practical/Practical_06.java
+++ OOP-Practical/Practical_06.java
@@ -1,3 +1,5 @@
+/*6: Write an application that accepts two doubles as its command-line arguments, multiplies these together, and display the product. */
+
 public class Practical_06 {
     public static void main(String[] args) {
         double num1 = 3.5;
