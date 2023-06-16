f451478ca29df5f8e41486d80bcb9dca64328875
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-06-16 10:05:24 +0530

    Update Practical_05.java

--- OOP-Practical/Practical_05.java
+++ OOP-Practical/Practical_05.java
@@ -1,3 +1,9 @@
+/*5: Write a program to print following triangle with character ‘A’ for n rows, n is to be passed as command line argument.
+A  A A A
+A  A A 
+A A
+A */
+
 public class Practical_05 {
     public static void main(String[] args) {
 
