6b821e2b7653d3e26d74702910c0e47ace6f83d4
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-06-16 10:00:55 +0530

    Update Practical_01.java

--- OOP-Practical/Practical_01.java
+++ OOP-Practical/Practical_01.java
@@ -1,3 +1,8 @@
+/*1	Pass student name using command line argument and print output in following format:     
+                First Name : abc
+                Second Name : pqr
+                Last Name : str */
+
 public class Practical_01 {
     public static void main(String[] args) {
         String firstName = "Prachi";
