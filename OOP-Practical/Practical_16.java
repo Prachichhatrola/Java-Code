e3ff0942650e611d397d1497249822ef951ea24a
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-06-17 08:50:39 +0530

    Update Practical_16.java

--- OOP-Practical/Practical_16.java
+++ OOP-Practical/Practical_16.java
@@ -1,3 +1,5 @@
+/*16: Write a program that will accept a string “java123is234fun” will replace all occurrences of digit with single space character in a new string. (use of replaceAll() function and regular expression for digit). Split the new formed string from space character and form the array of strings (use of split function). Display strings from the array in new line each. */
+
 public class Practical_16 {
     public static void main(String[] args) {
         String inputString = "java123is234fun";
