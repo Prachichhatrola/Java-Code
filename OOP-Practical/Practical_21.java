a315b6302106adb71a4080b41176c408fc19e2f3
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-06-17 08:53:01 +0530

    Update Practical_21.java

--- OOP-Practical/Practical_21.java
+++ OOP-Practical/Practical_21.java
@@ -1,3 +1,5 @@
+/*21: A method named average() has one argument that is an array of strings. It converts these to double values and returns their average. Write a program that illustrates how to declare and use this method. */ 
+
 public class Practical_21 {
     public static void main(String[] args) {
         String[] stringValues = { "10.5", "20.3", "15.7", "18.2" };
