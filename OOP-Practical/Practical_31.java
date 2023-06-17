01a985822982a445daeb3ef1d641a48764aeab8c
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-06-17 08:56:56 +0530

    Update Practical_31.java

--- OOP-Practical/Practical_31.java
+++ OOP-Practical/Practical_31.java
@@ -1,3 +1,5 @@
+/*31: Write an application that illustrates how a method can invoke a superclass method. Class I2 is extended by J2.Class J2 is extended by K2.Each of these classes defines a getDescription() method that returns a string. That string includes a description of the class plus descriptions of each superclass. Instantiate each of these classes and invoke the getDescription() method */
+
 class I2 {
     public String getDescription() {
         return "I2";
