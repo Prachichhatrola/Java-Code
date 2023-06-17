98d791cbcf519423af8fc799ed7139d4d2430349
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-06-17 08:56:35 +0530

    Update Practical_30.java

--- OOP-Practical/Practical_30.java
+++ OOP-Practical/Practical_30.java
@@ -1,3 +1,5 @@
+/*30: Write a program that illustrates method overriding. Class Bond is extended by ConvertibleBond. Each of there classes defines a display() method that outputs the string “Bond” of “ConvertibleBond”, respectively. Declare an array to hold six Bond objects. Initialize the elements of the array with a mix of Bond and ConvertibleBond objects. Execute a program loop to invoke the display method of each object. */
+
 class Bond {
     public void display() {
         System.out.println("Bond");
