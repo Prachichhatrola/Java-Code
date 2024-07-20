f77305e7430b94017bd919429f7084be5f4d0be5
Prachi Chhatrola <prachichhatrola009@gmail.com>
2024-07-20 21:28:29 +0530

    Update pattern7.java

--- Pattern/pattern7.java
+++ Pattern/pattern7.java
@@ -4,7 +4,8 @@
 
 public class Solution {
     public static void nStarTriangle(int n) {
-         for (int i = 1; i <= n; i++) {
+        
+       /*  for (int i = 1; i <= n; i++) {
             // Print spaces
             for (int j = i; j < n; j++) {
                 System.out.print(" ");
@@ -12,6 +13,22 @@ public class Solution {
             // Print stars
             for (int j = 1; j <= i * 2 - 1; j++) {
                 System.out.print("*");
+            }
+            System.out.println();
+        }  */
+
+        for(int i=0; i<n; i++){
+            //space
+            for(int j=0;j<n-i-1;j++){
+                System.out.print(" ");
+            }
+            //star
+            for(int j=0;j<2*i+1;j++){
+                System.out.print("*");
+            }
+            //space
+            for(int j=0;j<n-i-1;j++){
+                System.out.print(" ");
             }
             System.out.println();
         }
