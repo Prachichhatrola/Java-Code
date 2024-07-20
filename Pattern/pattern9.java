63e258258f66df82abc7150cac26de1e6d8a630a
Prachi Chhatrola <prachichhatrola009@gmail.com>
2024-07-20 22:03:59 +0530

    Create pattern9.java

---
+++ Pattern/pattern9.java
@@ -0,0 +1,34 @@
+public class Solution {
+    public static void nStarDiamond(int n) {
+        for(int i=0; i<n; i++){
+            //space
+            for(int j=0;j<n-i-1;j++){
+                System.out.print(" ");
+            }
+            //star
+            for(int j=0;j<2*i+1;j++){
+                System.out.print("*");
+            }
+            //space
+            for(int j=0;j<n-i-1;j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        for(int i=0;i<n;i++){
+            //space
+            for(int j=0;j<i;j++){
+                System.out.print(" ");
+            }
+            //star
+            for(int j=0; j<2*n-(2*i + 1); j++){
+                System.out.print("*");
+            }
+            //space
+            for(int j=0;j<i;j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
