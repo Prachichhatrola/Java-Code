f16a5f94d82aebc087a3987165fc8cd5a8610720
Prachi Chhatrola <prachichhatrola009@gmail.com>
2024-07-20 21:57:11 +0530

    Update pattern8.java

--- Pattern/pattern8.java
+++ Pattern/pattern8.java
@@ -4,7 +4,7 @@
 
 public class Solution {
     public static void nStarTriangle(int n) {
-        for (int i = 0; i < n; i++) {
+    /*    for (int i = 0; i < n; i++) {
             // Print leading spaces
             for (int j = 0; j < i; j++) {
                 System.out.print(" ");
@@ -13,6 +13,22 @@ public class Solution {
             for (int j = 0; j < 2 * (n - i) - 1; j++) {
                 System.out.print("*");
             }      
+            System.out.println();
+        }  */
+
+        for(int i=0;i<n;i++){
+            //space
+            for(int j=0;j<i;j++){
+                System.out.print(" ");
+            }
+            //star
+            for(int j=0; j<2*n-(2*i + 1); j++){
+                System.out.print("*");
+            }
+            //space
+            for(int j=0;j<i;j++){
+                System.out.print(" ");
+            }
             System.out.println();
         }
     }
