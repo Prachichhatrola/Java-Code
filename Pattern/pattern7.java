1c35cad1aa8f05fe32d322f243ed5c2a90e744b3
Prachi Chhatrola <prachichhatrola009@gmail.com>
2024-07-20 20:32:05 +0530

    Create pattern7.java

---
+++ Pattern/pattern7.java
@@ -0,0 +1,19 @@
+//   *
+//  ***
+// *****
+
+public class Solution {
+    public static void nStarTriangle(int n) {
+         for (int i = 1; i <= n; i++) {
+            // Print spaces
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int j = 1; j <= i * 2 - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
