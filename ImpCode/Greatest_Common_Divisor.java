6b37a90b4e560c3cfa7d759d17c5c4e6f270709a
Prachi Chhatrola <prachichhatrola009@gmail.com>
2024-07-21 13:20:34 +0530

    Create Greatest_Common_Divisor.java

---
+++ ImpCode/Greatest_Common_Divisor.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public static int gcd(int a, int b) {
+        while (a > 0 && b > 0) {
+            if (a > b) {
+                a = a % b;
+            } else {
+                b = b % a;
+            }
+        }
+        return (a == 0) ? b : a;
+    }
+}
