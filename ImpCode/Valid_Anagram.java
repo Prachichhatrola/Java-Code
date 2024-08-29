a62125d9e9fd01fb39f02a9ce0e009d31b972334
Prachi Chhatrola <prachichhatrola009@gmail.com>
2024-08-29 09:38:40 +0530

    Create Valid_Anagram.java

---
+++ ImpCode/Valid_Anagram.java
@@ -0,0 +1,13 @@
+//An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+        
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+        
+        return Arrays.equals(sChars, tChars);
+    }
+}
