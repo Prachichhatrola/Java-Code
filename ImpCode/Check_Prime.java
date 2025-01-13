329e02aa851924053e5e3e7d7d28d78b811cbc75
Prachi Chhatrola <prachichhatrola009@gmail.com>
2025-01-13 16:08:36 +0530

    Update Check_Prime.java

--- ImpCode/Check_Prime.java
+++ ImpCode/Check_Prime.java
@@ -23,3 +23,19 @@ public class Solution {
         return true;
     }
 }
+
+/* 
+class Main {
+    public static void main(String[] args) {
+        int num = 29;
+        boolean isPrime = true;
+        for(int i=2;i<num;i++){
+            if(num%i==0){
+                isPrime = false;
+                break;
+            }
+        }
+        System.out.println(isPrime);
+    }
+} 
+*/
